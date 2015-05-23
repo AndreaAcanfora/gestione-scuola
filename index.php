7430d5162cd1af57e2a2d8cf5f9e9bbd2f8dd215
Andrea Acanfora <andreaacanfora95@gmail.com>
2015-05-23 18:41:54 +0200

    rifatta index

--- index.php
+++ index.php
@@ -5,15 +5,22 @@ $_SESSION['nome'] = '';
 <!DOCTYPE html>
 <html>
   <?php include 'head.php';?>
-  <body>
-      <form  method="POST" action="execute.php" class="login">
-          <div id="container">
-              <input type="text" name="name" id="name" placeholder="Username"></tr><br>
-              <input type="password" name="password" id="password" placeholder="Password"><br>
-              <button id="link" type="submit" >LOGIN</a>
-              <div id="result"></div>
-          <div>
-      </form>
+  <body id="backgroundIndex">
+    <form  method="POST" action="execute.php" class="login">
+      <input type="text" name="name" id="indexLogin" placeholder="Username"></tr>
+      <input type="password" name="password" id="indexLogin" placeholder="Password">
+      <button id="bntLogin" type="submit" >LOGIN</a></button>
+      <a href="https://www.facebook.com/isisdavinci?fref=ts" id="fb"><i class="icon-facebook"></i></a>
+    </form>
+    <div id="containerText">
+    <p>
+      Benvenuto nella sezione amministrativa<br>
+      dell'ISIS Leonardo Da Vinci<br>
+      Da qui potrai gestire comodamente<br>
+      gli alunni della scuola;<br>
+      accedi all'area riservata.
+    </p>
+    </div>
   </body>
 </html>
 
